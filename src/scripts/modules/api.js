b6c8aab60338cdd6369b6c0ebffb284b189b92b0
Olya Zakharova <olyazakharova@MacBook-Pro-Olya.local>
2019-12-19 21:29:17 +0300

    upload

---
+++ src/scripts/modules/api.js
@@ -0,0 +1,68 @@
+export class Api {
+  constructor(config) {
+    this.url = config.url;
+    this.userUrl = config.userUrl;
+    this.headers = config.headers;
+  }
+
+  getResponseJson(res) {
+    if (res.ok) {
+      return res.json();
+    }
+    return Promise.reject(`Ошибка: ${res.status}`);
+  }
+
+  patchPromise(name, about) {
+    return fetch(this.url + this.userUrl, {
+      method: 'PATCH',
+      headers: this.headers,
+      body: JSON.stringify({
+        name,
+        about,
+      }),
+    })
+      .then((res) => this.getResponseJson(res));
+  }
+
+  patchData(name, job) {
+    this.patchPromise(name, job)
+      .then((result) => result)
+      .catch((err) => console.log(err));
+  }
+
+  getUserInfo() {
+    return fetch(this.url + this.userUrl, {
+      headers: this.headers,
+    })
+      .then((res) => this.getResponseJson(res))
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  getInitialCards() {
+    return fetch(this.url + this.userUrl, {
+      headers: this.headers,
+    })
+      .then((res) => this.getResponseJson(res))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  editUserInfo() {
+    return fetch(this.url + this.userUrl, {
+      headers: this.headers,
+    })
+      .then((res) => this.getResponseJson(res))
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+}
